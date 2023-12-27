7bfec64bedfabd08da34d4e50f6c619af488df5e
Josh Bostick <jbostick99@gmail.com>
2023-12-27 13:01:23 -0600

    finished

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var depAmount = double.Parse(Console.ReadLine());
+            account.Deposit(depAmount);            
+            
+            Console.WriteLine($"Your balance is now ${account.GetBalance()}.");
+            
         }
     }
 }
